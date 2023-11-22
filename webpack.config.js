7f98ed4943d1e3178ca4fcc8e11b7dc4c316c80b
Kulov_Olga <kulov_olga@dev-profinyanya.ru>
2023-11-22 23:59:31 +0400

    add article page

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ const devtool = devMode ? "source-map" : undefined;
 const entryPoints = {
   index: path.resolve(__dirname, "src", "index.js"),
   payment: path.resolve(__dirname, "src", "index.js"),
+  article: path.resolve(__dirname, "src", "index.js"),
 
   // Добавьте другие страницы здесь
 };
