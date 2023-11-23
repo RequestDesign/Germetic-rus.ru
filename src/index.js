87466785b6cdade4639108f533e29a6145818d47
Kulov_Olga <kulov_olga@dev-profinyanya.ru>
2023-11-23 12:51:28 +0400

    feat: add map payment page

--- src/index.js
+++ src/index.js
@@ -3,3 +3,10 @@ import "./index.scss";
 import "./js/components/header";
 import { example } from "./js/utils/constants";
 console.log(example);
+
+import map_delivery from "./js/components/map_delivery";
+
+
+try{
+    map_delivery()
+} catch {}
