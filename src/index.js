8bc006f631d2c8e52377552aecfb645d5495937f
Kulov_Olga <kulov_olga@dev-profinyanya.ru>
2023-11-23 17:42:58 +0400

    feat: update article page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,7 @@
 import "./index.scss";
-// import "./js/pages/home";
 import "./js/components/header";
 import { example } from "./js/utils/constants";
-console.log(example);
-
-import map_delivery from "./js/components/map_delivery";
+import "./js/components/main";
 
 
-try{
-    map_delivery()
-} catch {}
+
