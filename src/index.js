1c1a470245fa3a7fe15d29772bc52c882a51c509
Kulov_Olga <kulov_olga@dev-profinyanya.ru>
2023-11-28 01:51:42 +0400

    feat: change main page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import "./index.scss";
 import 'swiper/swiper-bundle.css';
-import "./js/components/main";
 import "./js/components/main-a";
 import { example } from "./js/utils/constants";
 import "./js/components/mainKuloverova";
