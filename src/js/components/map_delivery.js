4b171b49dde38e34504af404d261f8466dcf82a0
Kulov_Olga <kulov_olga@dev-profinyanya.ru>
2023-11-29 17:37:45 +0400

    feat: add contact page

--- src/js/components/map_delivery.js
+++ src/js/components/map_delivery.js
@@ -6,7 +6,24 @@ const mapDelivery = () => {
           zoom: 17,
         });
 
-        let placemark = new ymaps.Placemark( [55.84956882425105,37.677268005525036], {}, {
+        let placemark = new ymaps.Placemark( [55.84956882425105,37.677268005525036], {
+          balloonContent: `
+          <div class="baloon">
+          <div class="baloon__img-box">
+            <img src="./assets/images/baloon.webp" alt="" />
+          </div>
+          <div class="baloon__info-box">
+            <p class="baloon__info-title text-sm">Адрес</p>
+            <p class="baloon__info-desk">
+              г. Москва ул. Красная сосна, 2, корп. 1, стр. 1, оф. 555,
+              МОСТРАНССКЛАД
+            </p>
+          </div>
+        </div>
+
+          
+          `
+        }, {
             iconLayout: 'default#image',
             iconImageHref: './assets/images/icons/placemark.svg',
             iconImageSize: [90, 90],
@@ -22,14 +39,12 @@ const mapDelivery = () => {
       map.controls.remove("rulerControl"); // удаляем контрол правил
     //   map.behaviors.disable(["scrollZoom"]); // отключаем скролл карты (опционально)
 
+
     map.geoObjects.add(placemark)
+    
+// placemark.balloon.open()
       }
-
-
- 
-
       ymaps.ready(init);
-
 
 }
 
